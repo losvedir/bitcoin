3fdadc5cc34d20c43120baa890703a0d1af81d40
Gabe Durazo <gabe@gabedurazo.com>
2015-02-07 11:52:59 -0500

    [GJD] Initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Coinbase: 
+// api key: c9c13c1c80f271819931ef2938ddfab8
+// api secret: NHAQ8T93Rbl7oXZIZ8CYdIWd8VARYVO6O0D87D+uoxxXEFSv52ErANLZMnmuO0tKOQHJhAvJmJkgu3BgJ0dVjA==
+
+fn main() {
+    println!("Hello, world!");
+}
